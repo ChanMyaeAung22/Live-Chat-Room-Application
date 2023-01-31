f12da29b821eab1e026255737de98b71248e764a
ChanMyaeAung <chanmyae.cma30@gmail.com>
2023-01-31 20:04:56 +0630

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyBY6nmhw256OIJLw5L4fMtOpKzy6o45L2k",
+    authDomain: "vue-blog-system-1d796.firebaseapp.com",
+    projectId: "vue-blog-system-1d796",
+    storageBucket: "vue-blog-system-1d796.appspot.com",
+    messagingSenderId: "737293996465",
+    appId: "1:737293996465:web:5b37e603f45dc664402d90"
+  };
+
+  firebase.initializeApp("firebaseConfig");
+  let db=firebase.firestore();
+  let timestamp=firebase.firestore.FieldValue.serverTimestamp;
+
+  export {db,timestamp}
