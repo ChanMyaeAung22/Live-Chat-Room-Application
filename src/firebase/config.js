c6425f0c02ddd69717ad31e0da31da9eac4845ce
ChanMyaeAung <chanmyae.cma30@gmail.com>
2023-02-01 21:48:05 +0630

    toggle two form component and firebase auth setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore"
+import "firebase/auth"
 const firebaseConfig = {
     apiKey: "AIzaSyBY6nmhw256OIJLw5L4fMtOpKzy6o45L2k",
     authDomain: "vue-blog-system-1d796.firebaseapp.com",
@@ -11,6 +12,7 @@ const firebaseConfig = {
 
 firebase.initializeApp("firebaseConfig");
 let db = firebase.firestore();
+let auth = firebase.auth();
 let timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { db, timestamp }
+export { db, timestamp, auth }
